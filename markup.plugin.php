6b55f7d757f4a6da0414bc79c8b8e963748b1275
rickc <USER@DOMAIN>
2008-05-20 16:18:59 +0000

    Plugin markUp - Initial import. Plugin provides simple tag insertion for Habari's publish editor.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/markup@229 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ markup.plugin.php
@@ -0,0 +1,52 @@
+<?php
+
+class MarkUp extends Plugin {
+
+  /**
+    * Required Plugin Information
+    **/
+  function info() {
+    return array(
+      'name' => 'markUp',
+      'license' => 'Apache License 2.0',
+      'url' => 'http://habariproject.org/',
+      'author' => 'Habari Community',
+      'authorurl' => 'http://habariproject.org/',
+      'version' => '0.1',
+      'description' => 'Adds easy tag insertion to Habari\'s editor',
+      'copyright' => '2008'
+    );
+  }
+
+
+	public function action_admin_header($theme)
+	{
+		if ( $theme->admin_page == 'publish' ) {
+			Stack::add( 'admin_header_javascript', $this->get_url() . '/markitup/jquery.markitup.pack.js' );
+			Stack::add( 'admin_header_javascript', $this->get_url() . '/markitup/sets/html/set.js' );
+			
+			Stack::add('admin_stylesheet', array($this->get_url() . '/markitup/skins/markitup/style.css', 'screen'));
+			Stack::add('admin_stylesheet', array($this->get_url() . '/markitup/sets/html/style.css', 'screen'));
+		}
+	}
+
+	public function action_admin_footer($theme) {
+		if ( $theme->admin_page == 'publish' ) {
+			echo <<<MARKITUP
+<script type="text/javascript">
+$(document).ready(function() {
+mySettings.resizeHandle= false;
+$("#content").markItUp(mySettings);
+});
+</script>
+MARKITUP;
+    }
+  }
+	
+}
+
+  public function action_update_check() {
+    Update::add( 'markUp', 'F695D390-2687-11DD-B5E1-2D6F55D89593',  $this->info->version );
+  }
+  
+?>
---
+++ readme.txt
@@ -0,0 +1,34 @@
+Plugin: markUp
+URL: http://habariproject.org
+Version: 0.1
+Author: Habari Project
+
+Purpose 
+
+markUp is based on the markItUp! jQuery plugin by Jay Salvat (http://markitup.jaysalvat.com/). It provides a simple html text editor for those of us who want the productivity improvements of not having to type in all our own html tags, but don't want to use a wysiwyg editor. 
+
+Out of the box markUp provides easy insertion of all header tags, paragraphs, unordered and ordered lists, images, and links.
+
+Requirements 
+
+Just activate it.
+
+Installation
+
+1. Copy the plugin directory into your user/plugins directory or the site's plugins directory.
+2. Go to the plugins page of your Habari admin panel.
+3. Click on the Activate button for markItUp.
+
+Uninstallation
+
+1. Got to the plugins page of your Habari admin panel.
+2. Click on the Deactivate button.
+3. Delete the markItUp directory from your user/plugins directory.
+
+Cleanup
+
+markItUp makes no changes to your database, so no cleanup is required.
+
+Changelog
+
+Version 0.1 - Initial release
